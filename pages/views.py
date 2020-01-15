6f1dafe6d6b4bef080021a93d200591313eccd56
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-15 14:36:47 -0500

    Added the login and register pages

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {}
+    return render(request, 'pages/index.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'pages/contact.html', context)
